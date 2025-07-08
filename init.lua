80b8b3c438112ffd3dda4231bafe8f3b63d0f30a
avempali <avempali@atlassian.com>
2025-07-07 17:03:02 -0700

    feat: add system clipboard copy/paste keymaps

--- init.lua
+++ init.lua
@@ -6,6 +6,12 @@ vim.keymap.set('n', '<leader>pe', vim.cmd.Ex, { desc = '[P]roject [E]xplorer' })
 vim.keymap.set('n', 'L', '$', { desc = 'End of line' })
 vim.keymap.set('n', 'H', '^', { desc = 'Start of line' })
 
+-- Copy/Paste
+vim.keymap.set('n', '<leader>y', '"+y', { desc = '[Y]ank to system clipboard' })
+vim.keymap.set('v', '<leader>y', '"+y', { desc = '[Y]ank to system clipboard' })
+vim.keymap.set('n', '<leader>p', '"+p', { desc = '[P]aste from system clipboard' })
+vim.keymap.set('v', '<leader>p', '"+p', { desc = '[P]aste from system clipboard' })
+
 -- Toggle b/w open panes
 vim.keymap.set('n', '<leader>t', '<C-w>w', { desc = 'Toggle b/w open panes' })
 
