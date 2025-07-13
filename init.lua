a9d48a1712d12cafd4284fb61901a77e74720231
avempali <avempali@atlassian.com>
2025-07-13 12:09:27 -0700

    chore: refactor: consolidate key mappings for copy/paste to reduce redundancy Combined individual key mappings for yank and paste in normal and visual modes into single mappings using table syntax for clarity and maintainability.

--- init.lua
+++ init.lua
@@ -7,10 +7,8 @@ vim.keymap.set({'n', 'v'}, 'L', '$', { desc = 'End of line' })
 vim.keymap.set({'n', 'v'}, 'H', '^', { desc = 'Start of line' })
 
 -- Copy/Paste
-vim.keymap.set('n', '<leader>y', '"+y', { desc = '[Y]ank to system clipboard' })
-vim.keymap.set('v', '<leader>y', '"+y', { desc = '[Y]ank to system clipboard' })
-vim.keymap.set('n', '<leader>p', '"+p', { desc = '[P]aste from system clipboard' })
-vim.keymap.set('v', '<leader>p', '"+p', { desc = '[P]aste from system clipboard' })
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = '[Y]ank to system clipboard' })
+vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', { desc = '[P]aste from system clipboard' })
 
 -- Toggle b/w open panes
 vim.keymap.set('n', '<leader>t', '<C-w>w', { desc = 'Toggle b/w open panes' })
