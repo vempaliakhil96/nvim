bb3ba980e542a3fb7da9586bba280e5c92fcbfc0
avempali <avempali@atlassian.com>
2025-11-06 00:59:58 +0530

    feat: initialize LazyVim with codecompanion plugin Add lazy-lock.json, lazyvim.json, and codecompanion.nvim configuration with custom RovoDev adapter support.

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,58 @@
+return {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    opts = {
+        adapters = {
+            acp = {
+                rovodev = function()
+                    local helpers = require("codecompanion.adapters.acp.helpers")
+                    return {
+                        name = "rovodev",
+                        type = "acp",
+                        formatted_name = "RovoDev",
+                        roles = {
+                            llm = "assistant",
+                            user = "user",
+                        },
+                        commands = {
+                            default = {
+                                "uv",
+                                "run",
+                                "--project",
+                                "/Users/avempali/Documents/repos/acra-python",
+                                "rovodev",
+                                "acp",
+                            },
+                        },
+                        defaults = {},
+                        parameters = {
+                            protocolVersion = 1,
+                            clientCapabilities = {
+                                fs = { readTextFile = true, writeTextFile = true },
+                            },
+                            clientInfo = {
+                                name = "CodeCompanion.nvim",
+                                version = "1.0.0",
+                            },
+                        },
+                        handlers = {
+                            setup = function(self)
+                                return true
+                            end,
+                            form_messages = function(self, messages, capabilities)
+                                return helpers.form_messages(self, messages, capabilities)
+                            end,
+                            on_exit = function(self, code) end,
+                        },
+                    }
+                end,
+            },
+        },
+        -- NOTE: The log_level is in `opts.opts`
+        opts = {
+            log_level = "DEBUG",
+        },
+    },
+}
